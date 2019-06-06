6b2d46840aac9a5d8558f3206f3debf7f44512f3
Wordbe <Wordbe@github.com>
2019-06-06 23:20:00 +0900

    board_delete modified

--- eatFirst/board_delete.php
+++ eatFirst/board_delete.php
@@ -6,9 +6,9 @@ $conn = dbconnect($host,$dbid,$dbpass,$dbname);
 
 $board_no = $_GET['board_no'];
 
-mysqli_query($conn, "set autocommit = 0");	// autocommit 해제
-mysqli_query($conn, "set transation isolation level serializable");	// isolation level 설정
-mysqli_query($conn, "begin");	// begins a transation
+mysqli_query($conn, "set autocommit = 0");	// autocommit 해제.
+mysqli_query($conn, "set transation isolation level serializable");	// isolation level 설정.
+mysqli_query($conn, "begin");	// begins a transation.
 
 $ret = mysqli_query($conn, "delete 
 							from board
@@ -16,7 +16,7 @@ $ret = mysqli_query($conn, "delete
 
 if(!$ret)
 {
-	mysqli_query($conn, "rollback"); // query 수행 실패. 수행 전으로 rollback
+	mysqli_query($conn, "rollback"); // query 수행 실패. 수행 전으로 rollback.
     msg('Query Error : '.mysqli_error($conn));
 }
 else
@@ -24,7 +24,7 @@ else
 	mysqli_query($conn, "set @cnt = 0");
 	mysqli_query($conn, "update board
 						 set review_no = @cnt:=@cnt+1;");
-	mysqli_query($conn, "commit"); // query 수행 성공. 수행 내역 commit
+	mysqli_query($conn, "commit"); // query 수행 성공. 수행 내역 commit.
     s_msg ('성공적으로 삭제 되었습니다');
     echo "<meta http-equiv='refresh' content='0;url=board_list.php'>";
 }
